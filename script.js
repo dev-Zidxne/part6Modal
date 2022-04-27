68c350be20c35f1b2092fd174453372c16394508
Zidane Innis <zidaneinnis@gmail.com>
2022-04-26 21:23:59 -0500

    Completed Modal Window Project
    In this update I add the functionality of pressing the "esc" on the keyboard to exit the modal windows.
    
    I did this using an if statement which was originally two if statements but simplified down into one using "&&" as seen in the final code.
    
    This exercise was a lot of fun and I look forward to doing more with key presses like this. The possibilities are truly endless!

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//Working with classes
+
 const modal = document.querySelector('.modal');
 
 const overlay = document.querySelector('.overlay');
@@ -9,7 +11,6 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
 const openModal = function () {
-  console.log('Button clicked');
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -25,3 +26,13 @@ for (let i = 0; i < btnsOpenModal.length; i++)
 btnCloseModal.addEventListener('click', closeModal);
 
 overlay.addEventListener('click', closeModal);
+
+//Handling an "Esc" Keypress Event
+
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
